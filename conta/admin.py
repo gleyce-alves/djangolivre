4b699a900c265f481aefc49637f6ad29cd68e81f
dhiulysoares <dhiulysoaresds@gmail.com>
2021-12-01 18:44:38 -0300

    admin das classes cadastro e conta

--- conta/admin.py
+++ conta/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from conta.models import Conta
 
-# Register your models here.
+class ContaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cadastro', 'agencia', 'numero_conta', 'saldo')
+    list_display_links = ('id',)
+    search_fields = ('id', 'cadastro', 'agencia', 'numero_conta', 'saldo',)
+
+admin.site.register(Conta, ContaAdmin)
--- projeto_final/urls.py
+++ projeto_final/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from rest_framework import routers
 from cadastro.views import CadastroViewSet
 from conta.views import ContaViewSet
+from django.contrib import admin
 
 router = routers.DefaultRouter()
 router.register("cadastro", viewset=CadastroViewSet)
 router.register("conta", viewset=ContaViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls)
 ]
