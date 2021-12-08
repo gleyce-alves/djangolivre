d698fd2bc838c85a361fb4021f467687ca12df60
Gleyce Alves <71646387+gleyce-alves@users.noreply.github.com>
2021-12-08 12:32:05 -0300

    Delete transferencia directory

--- transferencia/serializer.py
+++
@@ -1,9 +0,0 @@
-from .models import Transacao
-from rest_framework import serializers
-
-
-class TransferenciaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Transacao
-        fields = ['id', 'remetente',
-                  'destinatario', 'data', 'valor_transferido']
--- transferencia/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from transferencia.models import Transacao
-from transferencia.serializer import TransferenciaSerializer
-
-
-class TransferenciaViewSet(viewsets.ModelViewSet):
-    queryset = Transacao.objects.all()
-    serializer_class = TransferenciaSerializer
